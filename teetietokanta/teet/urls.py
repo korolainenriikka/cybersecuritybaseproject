ec1018b9334d75eca7ddff1883531c25288a4572
Riikka Korolainen <55230880+korolainenriikka@users.noreply.github.com>
2021-03-22 16:45:31 +0200

    tea migration applied, links between pages

--- teetietokanta/teet/urls.py
+++ teetietokanta/teet/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = 'teet'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('uusi/', views.addNew, name='addNew'),
+    path('uusi/', views.addNew, name='addnew'),
 ]
